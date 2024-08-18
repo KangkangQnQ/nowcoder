b3902ffa7a158d2882cd1c33b119cc66e603fcd0
LiKangkang <lkk945821255@outlook.com>
2024-08-18 16:32:16 +0800

    ignored

---
+++ 华为机试/HJ16.购物单.java
@@ -0,0 +1,20 @@
+
+/**
+ * @nc app=nowcoder id=f9c6f980eeec43ef85be20755ddbeaf4 topic=37 question=21239 lang=Java
+ * 2024-08-18 00:53:28
+ * https://www.nowcoder.com/practice/f9c6f980eeec43ef85be20755ddbeaf4?tpId=37&tqId=21239
+ * [HJ16] 购物单
+ */
+
+/** @nc code=start */
+
+import java.util.Scanner;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
+
+/** @nc code=end */
